588d70327b6baf1755e9b216c1a3ffcbaeff6886
Gia Bảo <letrangiabao14@gmail.com>
2026-07-09 23:03:07 +0700

    fix(ui): elevate tooltip content z layer to 100

--- components/ui/tooltip.tsx
+++ components/ui/tooltip.tsx
@@ -45,12 +45,12 @@ function TooltipContent({
         alignOffset={alignOffset}
         side={side}
         sideOffset={sideOffset}
-        className="isolate z-50"
+        className="isolate z-100"
       >
         <TooltipPrimitive.Popup
           data-slot="tooltip-content"
           className={cn(
-            'bg-foreground text-background z-50 w-fit max-w-xs origin-(--transform-origin)',
+            'bg-foreground text-background z-100 w-fit max-w-xs origin-(--transform-origin)',
             'data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2 data-[state=delayed-open]:animate-in data-[state=delayed-open]:fade-in-0 data-[state=delayed-open]:zoom-in-95 data-open:animate-in data-open:fade-in-0 data-open:zoom-in-95 data-closed:animate-out data-closed:fade-out-0 data-closed:zoom-out-95 rounded-lg px-2.5 py-1.5 text-sm will-change-transform',
             'data-instant:duration-0',
             'selection:bg-background selection:text-foreground',
